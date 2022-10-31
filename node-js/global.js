a00934199def6ee04698f9d074c743c7e20a46f8
sergey <dimadeliev@gmail.com>
2022-10-31 22:30:55 +0200

    nodejs

---
+++ node-js/global.js
@@ -0,0 +1,11 @@
+// console.log(global);
+
+// setTimeout(() => {
+//    console.log('Hellow');
+// },  5000);
+
+// console.log(__dirname);
+
+// console.log(__filename);
+
+console.log(`Hello, ${process.argv[2]}`);
